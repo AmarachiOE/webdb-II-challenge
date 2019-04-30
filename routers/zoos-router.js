ea48d7b854a6a2763310601742caa705526b1f2f
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-30 12:46:53 -0700

    GET, GET by ID, POST requests complete

--- routers/zoos-router.js
+++ routers/zoos-router.js
@@ -1,19 +1,79 @@
-const knex = require('knex');
-const zoosRouter = require('express').Router();
+const knex = require("knex");
+const zoosRouter = require("express").Router();
 
 const knexConfig = {
-    client: "sqlite3",
-    connection: {
-        filename: "./data/lambda.sqlite3"
-    },
-    useNullAsDefault: true,
-    debug: true
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  },
+  useNullAsDefault: true,
+  debug: true
 };
 
-const db = knex(knexConfig);
+const zoosDb = knex(knexConfig);
 
-zoosRouter.get('/', (req, res) => {
-    res.send("Start writing your code")
+// ============ GET ALL
+zoosRouter.get("/", (req, res) => {
+  zoosDb("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The zoos information could not be retrieved." });
+    });
+});
+
+// ============ GET BY ID
+zoosRouter.get("/:id", (req, res) => {
+  zoosDb("zoos")
+    .where({ id: req.params.id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ error: "This zoo could not be found." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "This zoo's information could not be retrieved." });
+    });
+});
+
+// ============ POST
+zoosRouter.post("/", (req, res) => {
+  const zoo = req.body;
+  if (!zoo || !zoo.name) {
+    res.status(400).json({
+      error: "You must include a zoo with a unique name."
+    });
+  } else {
+    zoosDb("zoos")
+      .insert(zoo, "id")
+      .then(results => {
+        res.status(200).json(results);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the zoo to the database"
+        });
+      });
+  }
+});
+
+// ============ DELETE
+zoosRouter.delete("/:id", (req, res) => {
+  res.send("Start writing your code");
+});
+
+// ============ UPDATE
+zoosRouter.put("/:id", (req, res) => {
+  res.send("Start writing your code");
 });
 
 module.exports = zoosRouter;
