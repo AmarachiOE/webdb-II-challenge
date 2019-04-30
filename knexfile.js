486d9f3d74a98cc779fc83d32277c0ac775ae8cf
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-30 16:06:46 -0700

    Restructured bears-router.js and related files to include and import data>helper>bearModel, dbConfig, and knewfile.js. Only bears-router utilizes these files. I left the zoos-router the original way

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/lambda.sqlite3'
+      filename: './data/lambda.sqlite3' // from root
     },
     useNullAsDefault: true, // new configuration for SQLite
     debug: true // logs sql command in terminal
