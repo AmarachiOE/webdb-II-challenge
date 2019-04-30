e9c4f9aab085aa35b26bd08a95506fdc2a8223c4
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-30 13:11:41 -0700

    PUT request complete

--- routers/zoos-router.js
+++ routers/zoos-router.js
@@ -50,7 +50,7 @@ zoosRouter.post("/", (req, res) => {
   const zoo = req.body;
   if (!zoo || !zoo.name) {
     res.status(400).json({
-      error: "You must include a zoo with a unique name."
+      error: "You must include a zoo with a name."
     });
   } else {
     zoosDb("zoos")
@@ -73,11 +73,9 @@ zoosRouter.delete("/:id", (req, res) => {
     .delete()
     .then(count => {
       if (count > 0) {
-        res
-          .status(200)
-          .json({
-            message: `${count} ${count > 1 ? "records" : "record"} deleted`
-          });
+        res.status(200).json({
+          message: `${count} ${count > 1 ? "records" : "record"} deleted`
+        });
       } else {
         res.status(404).json({ error: "This zoo could not be found." });
       }
@@ -91,7 +89,30 @@ zoosRouter.delete("/:id", (req, res) => {
 
 // ============ UPDATE
 zoosRouter.put("/:id", (req, res) => {
-  res.send("Start writing your code");
+  const zoo = req.body;
+  if (!zoo || !zoo.name) {
+    res.status(400).json({
+      error: "You must include a zoo with a name."
+    });
+  } else {
+    zoosDb("zoos")
+      .where({ id: req.params.id })
+      .update(req.body)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({
+            message: `${count} ${count > 1 ? "records" : "record"} updated`
+          });
+        } else {
+          res.status(404).json({ error: "This zoo could not be found." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while updating the zoo from the database"
+        });
+      });
+  }
 });
 
 module.exports = zoosRouter;
